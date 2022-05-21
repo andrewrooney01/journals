322526233a5de92f055d0ccbc2cfc95f89c25639
Andrew Rooney <andrewrooney01@gmail.com>
2022-05-20 18:29:03 -0700

    ready to work with database

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,37 +1,37 @@
-//const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler')
 
 // @ desc Get Goals
 // @route GET /api/goals/
 // @access Private
-const getGoals = (req,res) => {
+const getGoals = asyncHandler(async (req,res) => {
     res.status(200).json({ message: 'Get Goals' })
-}
+})
 
 // @ desc Set Goal
 // @route POST /api/goals/
 // @access
-const setGoal = (req,res) => {
+const setGoal = asyncHandler(async (req,res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a new text field')
     }
-    
+
     res.status(200).json({message: 'Set Goal'})
-}
+})
 
 // @ desc Update Goal
 // @route POST /api/goals/:id
 // @access
-const updateGoal = (req,res) => {
+const updateGoal = asyncHandler(async (req,res) => {
     res.status(200).json({message: 'Update Goal'})
-}
+})
 
 // @ desc Delete Goal
 // @route POST /api/goals/:id
 // @access
-const deleteGoal = (req,res) => {
+const deleteGoal = asyncHandler(async (req,res) => {
     res.status(200).json({message: 'Delete Goal'})
-}
+})
 
 
 module.exports = {
